67213c9359fbcc262411c6e3694851917acfb3c2
Rajendra Surada <rajendrabvm007@gmail.com>
2026-09-15 23:10:44 +0530

    Rajendra | Build Failure Fix

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
 
 import './App.css';
-import {Switch, Route} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
+
 import Header from './Components/Header/Header';
 import RandomPage from './Components/Routes/RandomPage';
 import PokemonInfoPage from './Components/Routes/PokemonInfoPage';
 import Search from './Components/Search/Search';
 import PageNotFound from './Components/GeneralComponets/PageNotFound';
 
-import {useLocation} from 'react-router-dom'
 function App() {
-  const location = useLocation()
   return (
     <div className="App">
       <Header />
-     
-    <Switch >
-      <Route path='/' exact>
+
+      <Switch>
+        <Route path="/" exact>
           <RandomPage />
         </Route>
-        <Route path='/info/:pokemon' >
+
+        <Route path="/info/:pokemon">
           <PokemonInfoPage />
         </Route>
+
         <Route path="/search" exact>
-            <Search />
+          <Search />
         </Route>
+
         <Route path="*">
           <PageNotFound />
         </Route>
-
-    </Switch>
-
-  
+      </Switch>
     </div>
   );
 }
